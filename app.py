db20d2f4982eaef4eb6611dc487cd6c45d9a9a1f
Kevin Mathy <k.mathy@hexanet.fr>
2017-10-24 22:52:56 +0200

    Cleanings, and bug correction

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask_apps import app
+from flask_apps import *
 
 if __name__ == '__main__':
     app.run(host=app.config['LISTENIP'], port=app.config['LISTENPORT'], debug=app.config['DEBUG'])
--- flask_apps/__init__.py
+++ flask_apps/__init__.py
@@ -2,3 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
+
+from flask_apps import Routes
