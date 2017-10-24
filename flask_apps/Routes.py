89b43f623afe21d3471f389990630b9280a68258
Kevin Mathy <k.mathy@hexanet.fr>
2017-10-24 22:45:16 +0200

    New design, and informations well formated

--- flask_apps/Routes.py
+++ flask_apps/Routes.py
@@ -1,8 +1,21 @@
 from flask_apps import app
-from flask import render_template
+from flask import render_template, request, redirect
 import requests
 from requests.auth import HTTPBasicAuth
 import datetime
+
+
+@app.template_filter('secondsToTime')
+def _jinja2_filter_datetime(seconds):
+    minutes = seconds%3600/60
+    hours = seconds/3600
+    return str(hours) + ":" + str(minutes)
+
+
+@app.template_filter('humanReadableTime')
+def _jinja2_filter_datetime(time):
+    datetimeObject = datetime.datetime.strptime(time, '%Y%m%dT%H%M%S')
+    return datetimeObject.strftime('%d/%m/%Y %H:%M')
 
 
 def getPlaceID(city):
@@ -23,25 +36,31 @@ def getJourneys(srcStation, dstStation, departureDateTime):
     return ret
 
 
-@app.route("/route/<string:from_place>/<string:to_place>")
+@app.route("/route/<string:from_place>/<string:to_place>", methods=['GET', 'POST'])
 def routes(from_place, to_place):
     places = []
+    print request.form
+
+    if 'otherway' in request.form:
+        print "otherway"
+        print to_place
+        print from_place
+        return redirect('/route/' + to_place + '/' + from_place)
 
     # Get places IDs
     fromPlaceIdResult = getPlaceID(from_place)
     fromPlaceID = fromPlaceIdResult[1]['places'][1]['id']
+    fromPlaceName = fromPlaceIdResult[1]['places'][1]['name']
 
     toPlaceIdResult = getPlaceID(to_place)
     toPlaceID = toPlaceIdResult[1]['places'][1]['id']
+    toPlaceName = toPlaceIdResult[1]['places'][1]['name']
 
-    places.append({'from_place': from_place, 'to_place': to_place,\
+    places.append({'from_place': fromPlaceName, 'to_place': toPlaceName,\
                     'from_place_id': fromPlaceID, 'to_place_id': toPlaceID,\
     })
 
-    #Ymd\TH:i
     date = datetime.datetime.today().strftime('%Y%m%d %R:%M')
-    print date
     journeysResult = getJourneys(fromPlaceID, toPlaceID, date)
-    print journeysResult
-    
-    return render_template("welcome.html", places=places, journeysResult=journeysResult)
+
+    return render_template("route.html", places=places, journeysResult=journeysResult)
